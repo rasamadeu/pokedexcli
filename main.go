2cbf1c964ce04ebf93ddc3c98f0dd3510a13d215
rasamadeu <rasamadeu1@hotmail.com>
2025-06-10 19:18:15 +0100

    add exit and help commands

--- main.go
+++ main.go
@@ -1,25 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	
-	// Create scanner pointer that reads from stdin
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		if err := scanner.Err(); err != nil {
-			fmt.Println(err)
-			return 
-		}
-		command := cleanInput(scanner.Text())[0]
-		fmt.Println("Your command was:", command)
-	}
-
+	replStart()
 	return
 }
